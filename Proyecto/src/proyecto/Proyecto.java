318e2e645ed7c0573ac99c6026c3f488035f0466
AxelAlza <axelalza1998@gmail.com>
2019-10-12 02:10:08 -0300

    Lel

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -5,32 +5,39 @@ import java.util.Scanner;
 public class Proyecto {
 
 	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
+		
 		Scanner dato = new Scanner(System.in);
+		int op2;
+		int op;
 
 		do {
+
 			System.out.println("Menu");
 			System.out.println("");
 			System.out.println("1-Habitaciones");
 			System.out.println("2-Clientes");
 			System.out.println("3-Reservas");
 			System.out.println("0-Salir");
-			int op2;
-			int op;
+
 			op = dato.nextInt();
 
-			switch (op) // Menu principal
-			{
-			case 1:
+			switch (op) { // MENU PRINCIPAL
+
+			case 1: // MENU HABITACIONES
+
 				System.out.println("1-Registrar");
 				System.out.println("2-Modificar");
 				System.out.println("3-Eliminar");
 				System.out.println("4-Listar todo");
 				System.out.println("5-Buscar");
 				System.out.println("0-Volver");
+
 				op2 = dato.nextInt();
-				switch (op2) // Habitaciones
-				{
+
+				switch (op2) { // SWITCH HABITACIONES
+
 				case 1:
 					System.out.println("Usted eligio Registrar");
 					break;
@@ -54,7 +61,8 @@ public class Proyecto {
 
 				}
 
-			case 2:
+			case 2: // MENU CLIENTES
+
 				System.out.println();
 				System.out.println("1-Registrar");
 				System.out.println("2-Modificar");
@@ -62,8 +70,11 @@ public class Proyecto {
 				System.out.println("4-Listar Todos");
 				System.out.println("5-Buscar por CI");
 				System.out.println("0-Volver");
+
 				op2 = dato.nextInt();
-				switch (op2) {
+
+				switch (op2) { // SWITCH CLIENTES
+
 				case 1:
 					System.out.println("Usted eligio Registrar");
 					break;
@@ -83,16 +94,19 @@ public class Proyecto {
 					System.out.println("Usted eligio Volver");
 					break;
 				default:
-					System.out.println("Usted no seleccion una opcion valida");
+					System.out.println("Usted selecciono una opcion valida");
 				}
 
-			case 3:
+			case 3: // MENU RESERVAS
+
 				System.out.println("1-Reservas");
 				System.out.println("2-Lista de habitaciones y clientes");
 				System.out.println("3-Consultado de habitaciones");
 				System.out.println("0-Volver");
+
 				op2 = dato.nextInt();
-				switch (op2) {
+
+				switch (op2) { // SWITCH RESERVAS
 
 				case 1:
 					System.out.println("Usted eligio Reservas");
