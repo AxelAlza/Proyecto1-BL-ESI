57265402990c3e6a65f3b99f40e8878052e8d098
EGLYOKO93 <emmi.ca18@gmail.com>
2019-10-13 15:36:01 -0300

    nanii

---
+++ Proyecto/src/proyecto/Habitaciones.java
@@ -0,0 +1,31 @@
+package proyecto;
+import java.util.Scanner;
+public class Habitaciones {
+Scanner dato = new Scanner(System.in);
+public int registrarnumero() {
+
+	
+int numero;
+System.out.println("Ingrese un numero de habitacion");
+numero = dato.nextInt();
+
+return numero;
+}
+public String registronombre() {
+String nombre;
+System.out.println("Ingrese el nombre de habitacion");
+nombre = dato.next();
+
+
+return nombre;
+
+
+
+
+
+	
+
+
+
+}
+}
